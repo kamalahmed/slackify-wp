eb2ed8e9f83f5288b6beb4f6ad4290cb1bb3519c
Kamal <kamalacca@gmail.com>
2024-03-06 20:35:43 +0400

    send notification only on new question and answer

--- slackify-wp.php
+++ slackify-wp.php
@@ -21,8 +21,7 @@ class Slickify_WP
     public function __construct()
     {
         add_action('woocommerce_new_order', [$this, 'send_slack_order_notification'], 10, 1);
-        add_action('save_post_dwqa-question', [$this, 'send_slack_forum_notification'], 10, 3);
-        add_action('save_post_dwqa-answer', [$this, 'send_slack_forum_notification'], 10, 3);
+        add_action('transition_post_status', [$this, 'send_slack_forum_notification'], 10, 3);
         $this->slack_webhook_url = defined('SLACK_WEBHOOK_URL') ? SLACK_WEBHOOK_URL : 'https://hooks.slack.com/services/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';
 
     }
@@ -34,7 +33,7 @@ class Slickify_WP
         $order = wc_get_order($order_id);
         $order_data = $order->get_data();
 
-        $message = "A new order has been placed on https://techydevs.com!\n";
+        $message = "A new order has been placed!\n";
         $message .= "Order ID: " . $order_data['id'] . "\n";
         $message .= "Order Total: " . $order_data['total'] . "\n";
         $message .= "Customer: " . $order_data['billing']['first_name'] . " " . $order_data['billing']['last_name'] . "\n";
@@ -50,49 +49,51 @@ class Slickify_WP
             'method'      => 'POST'
         );
 
-        $response = wp_remote_post($this->slack_webhook_url, $args);
+		wp_remote_post($this->slack_webhook_url, $args);
     }
 
 
-    function send_slack_forum_notification($post_id, $post, $update)
-    {
-        if ($update) {
-            return;
-        }
+    function send_slack_forum_notification($new_status, $old_status, $post): void {
 
         if ( !in_array($post->post_type, array('dwqa-answer', 'dwqa-question'))) {
             return;
         }
+		// send notification only on new post
+	    if ( 'publish' === $new_status && 'publish' !== $old_status ) {
 
-        $post_title = get_the_title($post_id);
-        $post_url = get_permalink($post_id);
-        $message = '';
-        switch ($post->post_type) {
+		    $post_title = get_the_title($post->ID);
+		    $excerpt = get_the_excerpt($post->ID);
+		    $post_url = get_permalink($post->ID);
+		    $message = '';
+		    switch ($post->post_type) {
 
-            case 'dwqa-question':
-                $message = "A new question has been posted on https://techydevs.com!\n";
-                $message.= "Question Title: ". $post_title. "\n";
-                $message.= "Question URL: ". $post_url. "\n";
-                break;
+			    case 'dwqa-question':
+				    $message = "A new question has been posted! \n";
+				    $message.= "Question Title: ". $post_title. "\n\n";
+				    $message.= "Question: \n". $excerpt. "\n\n";
+				    $message.= "Question URL: ". $post_url. "\n";
+				    break;
 
-            case 'dwqa-answer':
-                $message = "A new answer has been posted on https://techydevs.com!\n";
-                $message.= "Answer Title: *". $post_title. "*\n";
-                $message.= "Answer URL: ". $post_url. "\n";
-                break;
-        }
+			    case 'dwqa-answer':
+				    $message = "A new answer has been posted! \n";
+				    $message.= "Answer Title: *". $post_title. "*\n\n";
+				    $message.= "Answer: \n". $excerpt. "\n\n";
+				    $message.= "Answer URL: ". $post_url. "\n";
+				    break;
+		    }
 
 
-        $payload = json_encode([
-            'text' => $message,
-        ]);
+		    $payload = json_encode([
+			    'text' => $message,
+		    ]);
+			wp_remote_post($this->slack_webhook_url, [
+			    'headers' => ['Content-Type' => 'application/json; charset=utf-8'],
+			    'body' => $payload,
+			    'method' => 'POST',
+			    'data_format' => 'body',
+		    ]);
+		}
 
-        wp_remote_post($this->slack_webhook_url, [
-            'headers' => ['Content-Type' => 'application/json; charset=utf-8'],
-            'body' => $payload,
-            'method' => 'POST',
-            'data_format' => 'body',
-        ]);
     }
 }
 
