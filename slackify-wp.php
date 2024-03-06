ab58793bf4d2c21eed56c3762cdcb33c3e69462e
Kamal <kamalacca@gmail.com>
2024-03-06 21:11:58 +0400

    prevent direct access

--- slackify-wp.php
+++ slackify-wp.php
@@ -1,4 +1,7 @@
 <?php
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
 /*
  * Plugin Name:       Slickify WP
  * Plugin URI:        https://github.com/kamalahmed/slickify-wp
